11ee1346705a806c06692222fc98d23972aeb04e
Andres Lopez <andres_lopez82161@elpoli.edu.co>
2023-10-25 16:04:26 -0500

    Delete producto por Id

---
+++ PuntosLeonisa.Products/1.Domain/PuntosLeonisa.Products.Domain/Model/Usuario.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuntosLeonisa.Products.Domain.Model
+{
+    internal class Usuario : IDisposable
+    {
+        public string Id { get; set; }
+
+        public string Cedula { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Celular { get; set; }
+
+        public string Email { get; set; }
+
+        public string Estado { get; set; }
+
+        public string FechaCambioEstado { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+
+        public DateTime FechaActualizacion { get; set; }
+
+        public string Agencia { get; set; }
+
+        public string Empresa { get; set; }
+
+        public string contraseña { get; set; }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+}
