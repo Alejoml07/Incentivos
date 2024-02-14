bc90763b4070368af8d2a8b1b396c7f9cb6ca716
Andrés López Restrepo <andres_lopez82161@elpoli.edu.co>
2024-02-14 07:57:11 -0500

    cambios

---
+++ microservices/PuntosLeonisa.Products/2.Infrastructure/PuntosLeonisa.Infracture.Persistence.CosmoDb/Configuration/GestorDeContenidoConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using PuntosLeonisa.Products.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PuntosLeonisa.Products.Domain.Service.DTO.GestorDeContenido;
+
+namespace PuntosLeonisa.infrastructure.Persistence.Configuration
+{
+    public class GestorDeContenidoConfiguration : IEntityTypeConfiguration<GestorDeContenidoDto>
+    {
+        public void Configure(EntityTypeBuilder<GestorDeContenidoDto> builder)
+        {
+            builder.ToContainer("Banner")
+                .HasPartitionKey(e => e.Id).HasKey(e => e.Id);
+        }
+    }
+}
