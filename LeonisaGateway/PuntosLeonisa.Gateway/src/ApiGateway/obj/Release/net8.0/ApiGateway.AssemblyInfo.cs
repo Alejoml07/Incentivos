4d83afa8e6971970f068d7c4895be9624748d424
Andrés López Restrepo <andres_lopez82161@elpoli.edu.co>
2024-03-08 12:09:28 -0500

    cambios

--- LeonisaGateway/PuntosLeonisa.Gateway/src/ApiGateway/obj/Release/net8.0/ApiGateway.AssemblyInfo.cs
+++ LeonisaGateway/PuntosLeonisa.Gateway/src/ApiGateway/obj/Release/net8.0/ApiGateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b729aa011550a3661fe5f9d98ea66b806405da64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82cf739b257332d9ced9d159c853e5d377d770d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
