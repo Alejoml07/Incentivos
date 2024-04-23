21d7296213f58af8e8fc3491ae01996f29ea737f
Andres <Andres@LAPTOP-MNOFTJ7P>
2024-04-23 08:45:42 -0500

    cambios

--- LeonisaGateway/PuntosLeonisa.Gateway/src/ApiGateway/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
+++ LeonisaGateway/PuntosLeonisa.Gateway/src/ApiGateway/obj/Debug/net8.0/ApiGateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4056b2dad585df81cd1012bbfc07f6c9e868d717")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7177d2a47a4048bf4994ab27411da60107bb10cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
