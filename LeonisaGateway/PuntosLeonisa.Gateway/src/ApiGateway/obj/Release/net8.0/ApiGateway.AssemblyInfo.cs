1db61240f6f00c509c6817da51027bbada664c03
Andres <Andres@LAPTOP-MNOFTJ7P>
2024-04-23 07:42:01 -0500

    cambios

--- LeonisaGateway/PuntosLeonisa.Gateway/src/ApiGateway/obj/Release/net8.0/ApiGateway.AssemblyInfo.cs
+++ LeonisaGateway/PuntosLeonisa.Gateway/src/ApiGateway/obj/Release/net8.0/ApiGateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5eb6118fc29276537e2ba7dac29be342b7b92a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4056b2dad585df81cd1012bbfc07f6c9e868d717")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
