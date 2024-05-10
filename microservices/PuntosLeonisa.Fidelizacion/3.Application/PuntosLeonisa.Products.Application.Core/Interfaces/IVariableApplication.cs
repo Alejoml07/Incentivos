b66c9a1c56cdd8a39d81d04538c25d2b6efa7141
Andres <Andres@LAPTOP-MNOFTJ7P>
2024-05-10 15:28:27 -0500

    cambios

--- microservices/PuntosLeonisa.Fidelizacion/3.Application/PuntosLeonisa.Products.Application.Core/Interfaces/IVariableApplication.cs
+++ microservices/PuntosLeonisa.Fidelizacion/3.Application/PuntosLeonisa.Products.Application.Core/Interfaces/IVariableApplication.cs
@@ -5,7 +5,8 @@ namespace PuntosLeonisa.Fidelizacion.Application.Core.Interfaces
 {
     public interface IVariableApplication
     {
-        Task<GenericResponse<bool>> AddVariable(VariableDto value);  
+        Task<GenericResponse<bool>> AddVariable(VariableDto value);
+        Task<GenericResponse<bool>> AddVariables(VariableDto[] value);
         Task<GenericResponse<bool>> DeleteVariableById(string id);
         Task<GenericResponse<VariableDto>> GetVariableById(string id);
         Task<GenericResponse<IEnumerable<VariableDto>>> GetVariables();
